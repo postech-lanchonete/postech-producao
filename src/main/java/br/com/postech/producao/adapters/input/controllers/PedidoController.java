94a98f004f9dd174b6e31307fee0714b5947289a
Silva Daniel, SUB-SDC-NL-MBM-BNC <Daniel.Silva@swisscom.com>
2024-03-12 09:42:46 +0100

    fix code smell

--- src/main/java/br/com/postech/producao/adapters/input/controllers/PedidoController.java
+++ src/main/java/br/com/postech/producao/adapters/input/controllers/PedidoController.java
@@ -3,11 +3,9 @@ package br.com.postech.producao.adapters.input.controllers;
 import br.com.postech.producao.adapters.adapter.PedidoAdapter;
 import br.com.postech.producao.adapters.dto.PedidoResponseDTO;
 import br.com.postech.producao.adapters.dto.requests.MudarStatusRequestDto;
-import br.com.postech.producao.adapters.dto.requests.PedidoRequestDto;
 import br.com.postech.producao.business.usecases.UseCase;
 import br.com.postech.producao.core.entities.Pedido;
 import br.com.postech.producao.core.enums.StatusDoPedido;
-import br.com.postech.producao.core.enums.StatusPagamento;
 import br.com.postech.producao.drivers.web.PedidoAPI;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -15,9 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,16 +24,13 @@ import java.util.List;
 @RestController
 public class PedidoController implements PedidoAPI {
     private final UseCase<Pedido, List<Pedido>> pedidoBuscarTodosUseCase;
-    private final UseCase<Pedido, Pedido> criarUseCase;
     private final UseCase<MudarStatusRequestDto, Pedido> mudarStatusUseCase;
     private final PedidoAdapter pedidoAdapter;
 
     public PedidoController(@Qualifier("pedidoBuscarTodosUseCase") UseCase<Pedido, List<Pedido>> pedidoBuscarTodosUseCase,
-                            @Qualifier("pedidoCriarUseCase") UseCase<Pedido, Pedido> criarUseCase,
                             @Qualifier("pedidoMudarStatusUseCase") UseCase<MudarStatusRequestDto, Pedido> mudarStatusUseCase,
                             PedidoAdapter pedidoAdapter) {
         this.pedidoBuscarTodosUseCase = pedidoBuscarTodosUseCase;
-        this.criarUseCase = criarUseCase;
         this.mudarStatusUseCase = mudarStatusUseCase;
         this.pedidoAdapter = pedidoAdapter;
     }
