5d1800005ce1be8051853771a998bf2da5bd940b
Silva Daniel, SUB-SDC-NL-MBM-BNC <Daniel.Silva@swisscom.com>
2024-03-16 21:06:01 +0100

    muda verbo endpoint para mudar de status

--- src/main/java/br/com/postech/producao/adapters/input/controllers/PedidoController.java
+++ src/main/java/br/com/postech/producao/adapters/input/controllers/PedidoController.java
@@ -12,8 +12,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,7 +52,7 @@ public class PedidoController implements PedidoAPI {
 
 
     @Override
-    @PutMapping("/{id}/status")
+    @PatchMapping("/{id}/status")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public PedidoResponseDTO mudarStatus(@PathVariable Long id,
                                          @RequestParam(required = false) StatusDoPedido novoStatus) {
